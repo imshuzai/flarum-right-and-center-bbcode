d019359a952b8d0489d8b7ff0c770de2c0a97c45
imshuzai <75674647+imshuzai@users.noreply.github.com>
2021-10-15 17:06:03 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -12,15 +12,15 @@ use Flarum\Frontend\Document;
 use s9e\TextFormatter\Configurator;
 
 return [
-    (new Extend\Frontend('forum'))
-        ->content(function (Document $document) {
-            $document->head[] = '<link rel="stylesheet" type="text/css" href="/assets/extensions/YOURUSERNAME-nameofextension/styles.css">';
-        }),
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
-                '[bbcode="{TEXT1}"]{TEXT2}[/bbcode]',
-                '<span class="{TEXT1}">{TEXT2}</span>'
+                '[right]{TEXT}[/right]',
+                '<div align="right">{TEXT}</div>'
+            );
+            $config->BBCodes->addCustom(
+                '[center]{TEXT}[/center]',
+                '<div align="center">{TEXT}</div>'
             );
         })
 ];
